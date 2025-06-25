a896488a1eef46e42070ecfe94aa45aa9d0925d2
Thamindu Kalhara <20.kalhara@gmail.com>
2025-06-25 17:27:21 +0530

    backend dev v0.1

--- backend/src/config/firebase.js
+++ backend/src/config/firebase.js
@@ -1,15 +1,14 @@
-require('dotenv').config();
 const { initializeApp } = require('firebase/app');
 const { getFirestore, collection, doc, setDoc, getDocs, getDoc, updateDoc, deleteDoc, serverTimestamp } = require('firebase/firestore');
 
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
-};
+ const firebaseConfig = {
+   apiKey: "AIzaSyBSVH9yTOxxCaoyBVbs90YbzdbsMtsOonk",
+   authDomain: "cake-shop-43920.firebaseapp.com",
+   projectId: "cake-shop-43920",
+   storageBucket: "cake-shop-43920.firebasestorage.app",
+   messagingSenderId: "171699797625",
+   appId: "1:171699797625:web:c5e2252964cd68d7f91ea8"
+ };
 
 
 const app = initializeApp(firebaseConfig);
